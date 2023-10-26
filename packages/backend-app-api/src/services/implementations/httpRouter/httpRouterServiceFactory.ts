bf5b1d2d2bf3658e5202f393d7d72b357deb810d
Fredrik Adelöw <freben@gmail.com>
2023-10-26 15:54:24 +0200

    make coreServices.httpRouter configurable, add notFound
    Signed-off-by: Fredrik Adelöw <freben@gmail.com>

--- packages/backend-app-api/src/services/implementations/httpRouter/httpRouterServiceFactory.ts
+++ packages/backend-app-api/src/services/implementations/httpRouter/httpRouterServiceFactory.ts
@@ -15,12 +15,39 @@
  */
 
 import {
+  LifecycleService,
+  LoggerService,
+  RootConfigService,
   coreServices,
   createServiceFactory,
 } from '@backstage/backend-plugin-api';
-import { Handler } from 'express';
+import { Handler, RequestHandler, Router } from 'express';
 import PromiseRouter from 'express-promise-router';
+import { MiddlewareFactory } from '../../../http';
 import { createLifecycleMiddleware } from './createLifecycleMiddleware';
+
+/**
+ * @public
+ */
+export interface HttpRouterConfigureContext {
+  pluginRouter: Router;
+  middleware: MiddlewareFactory;
+  routes: RequestHandler;
+  rootConfig: RootConfigService;
+  logger: LoggerService;
+  lifecycle: LifecycleService;
+}
+
+function defaultGetPath(pluginId: string): string {
+  return `/api/${pluginId}`;
+}
+
+function defaultConfigure(context: HttpRouterConfigureContext): void {
+  const { pluginRouter, routes, middleware, lifecycle } = context;
+  pluginRouter.use(createLifecycleMiddleware({ lifecycle }));
+  pluginRouter.use(routes);
+  pluginRouter.use(middleware.notFound());
+}
 
 /**
  * @public
@@ -30,6 +57,14 @@ export interface HttpRouterFactoryOptions {
    * A callback used to generate the path for each plugin, defaults to `/api/{pluginId}`.
    */
   getPath?(pluginId: string): string;
+
+  /**
+   * Allows you to customize the creation of the plugin router, by attaching a
+   * set of middleware and the given routes in the desired order. If no
+   * configure callback is given, a default set of middleware are added around
+   * the routes, e.g. for 404 handling.
+   */
+  configure?(context: HttpRouterConfigureContext): void;
 }
 
 /** @public */
@@ -37,22 +72,72 @@ export const httpRouterServiceFactory = createServiceFactory(
   (options?: HttpRouterFactoryOptions) => ({
     service: coreServices.httpRouter,
     deps: {
+      config: coreServices.rootConfig,
+      logger: coreServices.rootLogger,
       plugin: coreServices.pluginMetadata,
       lifecycle: coreServices.lifecycle,
       rootHttpRouter: coreServices.rootHttpRouter,
     },
-    async factory({ plugin, rootHttpRouter, lifecycle }) {
-      const getPath = options?.getPath ?? (id => `/api/${id}`);
+    createRootContext({ config, logger, rootHttpRouter }) {
+      const { getPath = defaultGetPath } = options ?? {};
+
+      const middleware = MiddlewareFactory.create({ config, logger });
+
+      //   / [wrapper]
+      //      / [pluginsRouter]   the router where plugins get registered
+      //         /api/foo         foo plugin
+      //         /api/bar         bar plugin
+      //      /api/               404 handler
+
+      const wrapper = PromiseRouter();
+      rootHttpRouter.use('', wrapper);
+
+      const pluginsRouter = PromiseRouter();
+      wrapper.use(pluginsRouter);
+
+      // This is the overarching 404 handler for all plugin routes. We want to
+      // properly let the notFound middleware handle illegal but plugin-like
+      // paths, instead of letting it fall down to the index route of the root
+      // router. Failing to do so will for example make a request to
+      // /api/not-a-valid-plugin return the index.html page rather than a 404.
+      // The problem is that we can't know beforehand what exact path pattern
+      // that the adopter uses. So we try to ask for the empty plugin ID, and if
+      // that is nonempty (e.g. /api/), we put the notFound handler there.
+      const pluginsRoot = getPath('');
+      if (pluginsRoot !== '' && pluginsRoot !== '/') {
+        wrapper.use(pluginsRoot, middleware.notFound());
+      }
+
+      return {
+        pluginsRouter,
+        middleware,
+      };
+    },
+    async factory(
+      { config, logger, plugin, lifecycle },
+      { pluginsRouter, middleware },
+    ) {
+      const { getPath = defaultGetPath, configure = defaultConfigure } =
+        options ?? {};
+
       const path = getPath(plugin.getId());
+      const pluginRouter = PromiseRouter();
+      const routes = PromiseRouter();
 
-      const router = PromiseRouter();
-      rootHttpRouter.use(path, router);
+      pluginsRouter.use(path, pluginRouter);
 
-      router.use(createLifecycleMiddleware({ lifecycle }));
+      configure({
+        pluginRouter,
+        middleware,
+        routes,
+        rootConfig: config,
+        logger,
+        lifecycle,
+      });
 
       return {
         use(handler: Handler) {
-          router.use(handler);
+          routes.use(handler);
         },
       };
     },
